82acfc746866abed2b12b7b4d6855b57d2a50b31
AmTeKay <85189560+AmTeKay@users.noreply.github.com>
2021-06-27 22:26:44 +0630

    Day4_Treasure_map.py

---
+++ Day4_Treasure_map.py
@@ -0,0 +1,23 @@
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+x = list(position)
+# print(x[0])
+R = int(x[0]) - 1
+C = int(x[1]) - 1
+# print(C)
+# print(R)
+map[C][R] = "X"
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
